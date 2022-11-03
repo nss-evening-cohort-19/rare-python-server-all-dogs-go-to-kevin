ce22ca7455a7325a3d4d1035fd864b5b9504242b
Imad Ottallah <imadotta@gmail.com>
2022-11-02 19:19:52 -0500

    working on comments

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-from views.comment_requests import get_all_comments, get_single_comment
+from views.comment_requests import get_all_comments, get_single_comment, update_comment
 from views.post_requests import get_all_posts, get_single_post
 
 from views.user import create_user, login_user
@@ -94,7 +94,25 @@ class HandleRequests(BaseHTTPRequestHandler):
 
     def do_PUT(self):
         """Handles PUT requests to the server"""
-        pass
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        success = False
+
+        if resource == "comments":
+            success = update_comment(id, post_body)
+        # rest of the elif's
+
+        if success:
+            self._set_headers(204)
+        else:
+            self._set_headers(404)
+
+        self.wfile.write("".encode())
 
     def do_DELETE(self):
         """Handle DELETE Requests"""
---
+++ views/__init__.py
@@ -0,0 +1,1 @@
+from .comment_requests import update_comment
