d5da60be9fcc07f4498f927945660b0892e868dd
Emily Stroud <emilystroud93@gmail.com>
2022-11-02 19:23:55 -0500

    editing

--- views/comment_requests.py
+++ views/comment_requests.py
@@ -2,45 +2,6 @@ import sqlite3
 import json
 from models import Comment
 
-<<<<<<< HEAD
-COMMENTS = [
-    {
-      "id": 1,
-      "post_id": 2,
-      "author_id": 3,
-      "content" : "A Comment"
-    },
-    {
-      "id": 2,
-      "post_id": 3,
-      "author_id": 4,
-      "content" : "Another Comment"
-    }
-]
-
-def update_comment(id, new_comment):
-    with sqlite3.connect("./kennel.sqlite3") as conn:
-      db_cursor = conn.cursor()
-      
-      db_cursor.execute("""
-      UPDATE Comment
-          SET
-              post_id = ?,
-              author_id = ?,
-              content = ?
-      WHERE id = ?
-      """, (new_comment['id'], new_comment['post_id'], new_comment['author_id'], new_comment['content'], id, ))
-      
-      rows_affected = db.cusrsor.rowcount
-      
-      if rows_affected == 0:
-        
-        return False
-      
-      else:
-        
-        return True
-=======
 def get_all_comments():
     with sqlite3.connect("./db.sqlite3") as conn:
 
@@ -88,4 +49,3 @@ def get_single_comment(id):
         comment = Comment(data['id'], data['post_id'], data['author_id'], data['content'])
 
         return json.dumps(comment.__dict__)
->>>>>>> 2fb1433ed887ba12640a0b2c8f96901f7db281fc
