42ba9808f7c741c0485aafbf786a6b6b526cd613
Keaton Law <keaton.c.law@gmail.com>
2022-11-02 20:29:05 -0500

    indent

--- views/comment_requests.py
+++ views/comment_requests.py
@@ -61,15 +61,15 @@ def delete_comment(id):
 
 def update_comment(id, new_comment):
     with sqlite3.connect("./db.sqlite3") as conn:
-    db_cursor = conn.cursor()
-    db_cursor.execute("""
-    UPDATE Comments
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        UPDATE Comments
         SET
             post_id = ?,
             author_id = ?,
             content = ?
-    WHERE id = ?
-    """, (new_comment['post_id'], new_comment['author_id'], new_comment['content'], id, ))
+        WHERE id = ?
+        """, (new_comment['post_id'], new_comment['author_id'], new_comment['content'], id, ))
     rows_affected = db_cursor.rowcount
 
     if rows_affected == 0:
